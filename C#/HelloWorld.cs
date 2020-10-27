e88522aa7d7b9ce6c0ba0b466723c7cdba6efd95
Kritesh Sharma <kritesh.sharma@synapse.com>
2020-10-27 21:17:01 +0530

    C# helloworld file added

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
